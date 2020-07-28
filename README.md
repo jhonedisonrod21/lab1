fec58b3d50c4c8fa0457937325291edcd685c496
jhonedisonrod21 <jhonedisonrod21@gmail.com>
2020-07-28 18:15:09 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# titulo 1
+## subitulo
+este es un ejersiocio de prueba para sistemas distribuidos
---
+++ server.js
@@ -0,0 +1,4 @@
+const express = require('express');
+var server = express();
+server.use(express.static(__dirname+'/public'))
+server.listen(80);
